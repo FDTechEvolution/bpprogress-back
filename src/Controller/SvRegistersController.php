8fba74b261d97617d5dba1b8a92620691da0d104
artdata1 <48270726+artdata1@users.noreply.github.com>
2020-04-15 01:04:39 +0700

    14-04-63

--- src/Controller/SvRegistersController.php
+++ src/Controller/SvRegistersController.php
@@ -23,6 +23,7 @@ class SvRegistersController extends AppController
         $this->Users = TableRegistry::get('Users');
         $this->Otps = TableRegistry::get('user_otps');
         $this->loadComponent('User');
+        $this->loadComponent('Sms');
     }
 
     public function register(){
@@ -38,15 +39,15 @@ class SvRegistersController extends AppController
                 $user_register->password = $password;
 
                 if($this->Users->save($user_register)){
-                    $this->createOTP($user_register->id);
+                    $this->responData['data'] = $this->createOTP($user_register->id, $postData['mobile']);
                     $this->responData['status'] = 200;
                     $this->responData['msg'] = 'Registed...';
                 }else{
-                    $this->responData['msg'] = 'Failed...';
+                    $this->responData['msg'] = 'เกิดข้อผิดพลาดในการลงทะเบียน กรุณาลองใหม่ในภายหลัง...';
                 }
             }else{
                 $this->responData['status'] = 400;
-                $this->responData['msg'] = 'Mobile Dupplicate';
+                $this->responData['msg'] = 'หมายเลขโทรศัพท์นี้ ได้มีการลงทะเบียนแล้ว กรุณาตรวจสอบ...';
             }
             
         }
@@ -64,11 +65,36 @@ class SvRegistersController extends AppController
         }
     }
 
-    private function createOTP ($id) {
+    private function createOTP ($id,$mobile) {
         $otp = $this->Otps->newEntity();
         $otp->user_id = $id;
         $otp->otp_ref = substr(str_shuffle("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"), 0, 4);
         $otp->otp_code = substr(str_shuffle("0123456789"), 0, 6);
-        $this->Otps->save($otp);
+        if($this->Otps->save($otp)){
+            // return $otp_return = [$id, $otp->otp_ref];
+            $message = "รหัส OTP ของคุณคือ ".$otp->otp_code." รหัสอ้างอิง ".$otp->otp_ref." สำหรับลงทะเบียน BPprogress ระยะเวลาสิ้นสุด ".date('d-m-Y')." ";
+            return $this->Sms->sendOTP('0000', $mobile, $message);
+        }
+    }
+
+    public function checkOtp () {
+        if ($this->request->is(['post','ajax'])) {
+            $postData = $this->request->getData();
+            $otp = $this->Otps->find('all')->where(['user_id' => $postData['id'], 'otp_ref' => $postData['otp_ref'], 'otp_code' => $postData['otp_code']])->first();
+            if(sizeof($otp) > 0) {
+                $user_verify = $this->Users->get($postData['id']);
+                $user_verify->isverify = 'Y';
+                if($this->Users->save($user_verify)) {
+                    $this->responData['status'] = 200;
+                    $this->responData['msg'] = 'Verified';
+                }
+            }else{
+                $this->responData['status'] = 400;
+                $this->responData['msg'] = 'รหัส OTP ไม่ถูกต้อง กรุณาตรวจสอบ...';
+            }
+        }
+
+        $json = json_encode($this->responData, JSON_UNESCAPED_UNICODE);
+        $this->set(compact('json'));
     }
 }
