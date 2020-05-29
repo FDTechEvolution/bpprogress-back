58e3bb0156268257f851cfa6f3cdb51466743d44
artdata1 <48270726+artdata1@users.noreply.github.com>
2020-05-29 16:44:02 +0700

    29-05-63 : facebook login

--- src/Controller/SvLoginController.php
+++ src/Controller/SvLoginController.php
@@ -33,34 +33,59 @@ class SvLoginController extends AppController {
     public function login() {
         if ($this->request->is(['ajax', 'post'])) {
             $postData = $this->request->getData();
-            // $this->log($postData, 'debug');
-            if (isset($postData['mobile']) && isset($postData['password'])) {
-                $password = $postData['password'];
-                $mobile = $postData['mobile'];
+            $isfacebook = (isset($postData['isfacebook'])) ? $postData['isfacebook'] : 'N' ;
+            if($isfacebook == 'N') {
+                if (isset($postData['mobile']) && isset($postData['password'])) {
+                    $password = $postData['password'];
+                    $mobile = $postData['mobile'];
 
-                $user = $this->User->authenUser($mobile, $password);
-                if (is_null($user)) {
-                    $this->responData['status'] = 403;
-                    $this->responData['msg'] = 'หมายเลขโทรศัพท์ หรือ รหัสผ่าน ไม่ถูกต้อง!';
-                } else {
-                    if ($user->type == 'ADMIN' || $user->type == 'SELLER') {
-                        $authenCode = $this->User->generateAuthenCode($user->id);
-                        $user['authen_code'] = $authenCode;
-                        $this->responData['data'] = $user;
+                    $user = $this->User->authenUser($mobile, $password);
+                    if (is_null($user)) {
+                        $this->responData['status'] = 403;
+                        $this->responData['msg'] = 'หมายเลขโทรศัพท์ หรือ รหัสผ่าน ไม่ถูกต้อง!';
                     } else {
-                        if ($user->isverify != 'Y') {
-                            $getOTP = $this->Otps->find('all')->where(['user_id' => $user->id])->first();
-                            $this->responData['status'] = 404;
-                            $this->responData['data'] = [true, $getOTP->user_id, $getOTP->otp_ref];
-                        } else {
-                            // $user->password = NULL;
+                        if ($user->type == 'ADMIN' || $user->type == 'SELLER') {
+                            $authenCode = $this->User->generateAuthenCode($user->id);
+                            $user['authen_code'] = $authenCode;
                             $this->responData['data'] = $user;
+                        } else {
+                            if ($user->isverify != 'Y') {
+                                $getOTP = $this->Otps->find('all')->where(['user_id' => $user->id])->first();
+                                $this->responData['status'] = 404;
+                                $this->responData['data'] = [true, $getOTP->user_id, $getOTP->otp_ref];
+                            } else {
+                                // $user->password = NULL;
+                                $authenCode = $this->User->generateAuthenCode($user->id);
+                                $user['authen_code'] = $authenCode;
+                                $this->responData['data'] = $user;
+                            }
                         }
                     }
+                } else {
+                    $this->responData['status'] = 400;
+                    $this->responData['msg'] = 'post field(s) is invalid.';
                 }
-            } else {
-                $this->responData['status'] = 400;
-                $this->responData['msg'] = 'post field(s) is invalid.';
+            }else if($isfacebook == 'Y') {
+                $user = $this->Users->find()->where(['id' => $postData['id']])->first();
+                $this->log($user, 'debug');
+                if(is_null($user)) {
+                    $fb_register = $this->Users->newEntity();
+                    $fb_register->id = $postData['id'];
+                    $fb_register->fullname = $postData['fullname'];
+                    $fb_register->mobile = 0;
+                    $fb_register->isactive = 'Y';
+                    $fb_register->isverify = 'Y';
+                    $fb_register->type = 'NORMAL';
+                    if($this->Users->save($fb_register)) {
+                        $authenCode = $this->User->generateAuthenCode($fb_register->id);
+                        $fb_register['authen_code'] = $authenCode;
+                        $this->responData['data'] = $fb_register;
+                    }
+                }else{
+                    $authenCode = $this->User->generateAuthenCode($user->id);
+                    $user['authen_code'] = $authenCode;
+                    $this->responData['data'] = $user;
+                }
             }
         }
         
